3c2976d2062a6ee0befc4885ab251fa60fb43d99
T3anon <mark.swearingen2008@gmail.com>
2026-04-27 20:03:24 -0400

    google sign in fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ import { prisma } from "@/lib/prisma";
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as any,
   secret: process.env.NEXTAUTH_SECRET,
-  debug: false,
+  debug: true,
   session: {
     strategy: "jwt",
   },
@@ -61,6 +61,13 @@ export const authOptions: NextAuthOptions = {
         (session.user as any).username = token.username;
       }
       return session;
+    },
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
     },
   },
   events: {
